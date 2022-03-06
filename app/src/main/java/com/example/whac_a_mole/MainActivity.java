4e88ce6575822ea438e49ec92c9a7c00acbf32e2
David <71563428+Darodbas@users.noreply.github.com>
2022-03-06 22:18:38 +0100

    música añadida, info añadida y sppiners dificultad

--- app/src/main/java/com/example/whac_a_mole/MainActivity.java
+++ app/src/main/java/com/example/whac_a_mole/MainActivity.java
@@ -1,18 +1,56 @@
 package com.example.whac_a_mole;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     protected Button btJugar, btJugar2;
 
+    protected ImageView ivInfoPasaportes, ivInfoFacturacion;
+    protected Spinner spDifPasaportes, spDifFacturacion;
+    protected String[] nivelesDificultad={"Facil","Intermedio","Difícil"};
+
+
+    public AlertDialog MensajeInfo(int modo){
+
+        String mensaje,titulo;
+
+        if(modo==1){
+            titulo="Pasaportes";
+            mensaje="En este modo deberás visar correctamente los pasaportes. Hay 2 pasaportes validos y uno no válido. Para aceptar o rechazar los pasaportes puedes cambiar la tinta del sello entre rojo o verde";
+        }else{
+            titulo="Facturación";
+            mensaje="explicación facturación";
+        }
+
+            AlertDialog.Builder mi_builder = new AlertDialog.Builder
+                    (MainActivity.this);
+            mi_builder.setTitle(titulo)
+                    .setMessage(mensaje)
+                            .setPositiveButton("ACEPTAR",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick (DialogInterface dialog,
+                                                             int which)
+                                        {
+                                            //no se hace nada
+                                        }
+                                    });
+            return mi_builder.create();
+
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +61,22 @@ public class MainActivity extends AppCompatActivity {
 
         btJugar = findViewById(R.id.btJugar);
         btJugar2 = findViewById(R.id.btJugar2);
+        ivInfoFacturacion = findViewById(R.id.ivInfoFacturacion);
+        ivInfoPasaportes = findViewById(R.id.ivInfoPasaportes);
+        spDifFacturacion = findViewById(R.id.spDificultadcheckin);
+        spDifPasaportes = findViewById(R.id.spDificultadPasaportes);
+
 
         MediaPlayer mpMusica= MediaPlayer.create (MainActivity.this, R.raw.menuinicio);
         mpMusica.setVolume(0.75f, 0.75f);
         mpMusica.setLooping(true);
         mpMusica.start();
+
+        //adaptador spinner//
+        ArrayAdapter mi_adaptador = new ArrayAdapter(this, android.R.layout.simple_spinner_item,nivelesDificultad);
+        //asociamos adaptador
+        spDifPasaportes.setAdapter(mi_adaptador);
+        spDifFacturacion.setAdapter(mi_adaptador);
 
 
 
@@ -84,5 +133,23 @@ public class MainActivity extends AppCompatActivity {
                 mpMegafonia.release();
             }
         });
+
+        ivInfoPasaportes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog dialogoPas = MensajeInfo(1);
+                dialogoPas.show();
+            }
+        });
+
+        ivInfoFacturacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog dialogoCheck = MensajeInfo(2);
+                dialogoCheck.show();
+
+            }
+        });
+
     }
 }
