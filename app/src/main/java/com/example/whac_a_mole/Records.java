5e1fa2a71197da18b4ca24ced05728ff0a63e67f
David <71563428+Darodbas@users.noreply.github.com>
2022-03-21 10:58:01 +0100

    Records mejorados para Facturación (Tiempo en minutos y segundos) (Error clics fuera de pantalla)

--- app/src/main/java/com/example/whac_a_mole/Records.java
+++ app/src/main/java/com/example/whac_a_mole/Records.java
@@ -81,23 +81,26 @@ public class Records extends AppCompatActivity {
         }
 
         if(records.getLong("TIEMP1",-1)!=-1) {
-            tvCheckInPuntuacion[0].setText("Tiempo: "+String.valueOf(records.getLong("TIEMP1",-1)));
+           // tvCheckInPuntuacion[0].setText("Tiempo: "+String.valueOf(records.getLong("TIEMP1",-1))+" s"); // El tiempo se muestra en minutos y segundo
+            tvCheckInPuntuacion[0].setText("Tiempo: "+String.valueOf(records.getLong("TIEMP1",-1)/60)+":"+String.valueOf(records.getLong("TIEMP1",-1)%60));
         }
         if(records.getLong("TIEMP2",-1)!=-1) {
-            tvCheckInPuntuacion[1].setText("Tiempo: "+String.valueOf(records.getLong("TIEMP2",-1)));
+            //tvCheckInPuntuacion[1].setText("Tiempo: "+String.valueOf(records.getLong("TIEMP2",-1))+" s");
+            tvCheckInPuntuacion[1].setText("Tiempo: "+String.valueOf(records.getLong("TIEMP2",-1)/60)+":"+String.valueOf(records.getLong("TIEMP2",-1)%60));
         }
         if(records.getLong("TIEMP3",-1)!=-1) {
-            tvCheckInPuntuacion[2].setText("Tiempo: "+String.valueOf(records.getLong("TIEMP3",-1)));
+            //tvCheckInPuntuacion[2].setText("Tiempo: "+String.valueOf(records.getLong("TIEMP3",-1))+" s");
+            tvCheckInPuntuacion[2].setText("Tiempo: "+String.valueOf(records.getLong("TIEMP3",-1)/60)+":"+String.valueOf(records.getLong("TIEMP3",-1)%60));
         }
 
         if(records.getInt("NUMC1",-1)!=-1) {
-            tvCheckInClicks[0].setText("Num. Clicks: "+Integer.toString(records.getInt("NUMC1",-1)));
+            tvCheckInClicks[0].setText("Clics: "+Integer.toString(records.getInt("NUMC1",-1)));
         }
         if(records.getInt("NUMC2",-1)!=-1) {
-            tvCheckInClicks[1].setText("Num. Clicks: "+Integer.toString(records.getInt("NUMC2",-1)));
+            tvCheckInClicks[1].setText("Clics: "+Integer.toString(records.getInt("NUMC2",-1)));
         }
         if(records.getInt("NUMC3",-1)!=-1) {
-            tvCheckInClicks[2].setText("Num. Clicks: "+Integer.toString(records.getInt("NUMC3",-1)));
+            tvCheckInClicks[2].setText("Clics: "+Integer.toString(records.getInt("NUMC3",-1)));
         }
 
         ivSalirRecords.setOnClickListener(new View.OnClickListener() {
